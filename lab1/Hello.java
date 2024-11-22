131d5b62fc6806a1116f165babd0f8b14f5acf98
Pattararnxx <pattaraporn.po@kkumail.com>
2024-11-22 10:39:57 +0700

    update

---
+++ lab1/Hello.java
@@ -0,0 +1,6 @@
+package lab1;
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Pattaraporn Potjanapimol, and student ID is 673040627-5");        
+    }
+}
