6cd87200bf41bcc71fd2114babc3b8172bbac1d1
Pattararnxx <pattaraporn.po@kkumail.com>
2025-02-07 10:03:38 +0700

    fix and inclass9

--- potjanapimol/pattaraporn/lab8/MobileDeviceV3.java
+++ potjanapimol/pattaraporn/lab8/MobileDeviceV3.java
@@ -11,95 +11,108 @@
  * Uses a structured approach 
  * to organize menus, submenus, and menu items.
  * 
- * Last-Update : 04/02/2568
+ * Last-Update : 07/02/2568
  */
 
-package potjanapimol.pattaraporn.lab8;
+ package potjanapimol.pattaraporn.lab8;
 
-import javax.swing.*;
-
-public class MobileDeviceV3 extends MobileDeviceV2 {
-    // Declare the JMenuBar and JMenu objects for menu and submenus
-    protected JMenuBar menuBar;
-    protected JMenu fileMenu, configMenu, sizeMenu, colorMenu, fontMenu;
-
-    // Declare JMenuItem objects for each menu item
-    protected JMenuItem newItem, openItem, saveItem, exitItem;
-
-    // Constructor that passes title to the superclass
-    public MobileDeviceV3(String title) {
-        super(title);
-    }
-
-    // Method to create and show the GUI
-    public static void createAndShowGUI() {
-        MobileDeviceV3 mdv3 = new MobileDeviceV3("Mobile Device V3");
-
-        mdv3.addComponents(); // Add components like buttons and menus
-        mdv3.setFrameFeature(); // Set frame properties like size, close operation, etc.
-    }
-
-    // Main method to invoke the createAndShowGUI method in the Event Dispatch
-    // Thread
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
-
-    // Override addComponents to add the menu bar to the existing layout
-    @Override
-    protected void addComponents() {
-        super.addComponents(); // Call the superclass method to retain existing components
-        addMenus(); // Add the menu bar and menus
-        this.setJMenuBar(menuBar); // Set the created menu bar to the frame
-    }
-
-    // Method to create the menu bar and add menus/submenus
-    protected void addMenus() {
-        menuBar = new JMenuBar(); // Create a new menu bar
-
-        // Create and add File menu with items
-        fileMenu = new JMenu("File");
-        newItem = new JMenuItem("New");
-        openItem = new JMenuItem("Open");
-        saveItem = new JMenuItem("Save");
-        exitItem = new JMenuItem("Exit");
-
-        fileMenu.add(newItem);
-        fileMenu.add(openItem);
-        fileMenu.add(saveItem);
-        fileMenu.add(exitItem);
-
-        // Create and add Config menu with submenus (Size, Color, Font)
-        configMenu = new JMenu("Config");
-
-        sizeMenu = new JMenu("Size");
-        sizeMenu.add(new JMenuItem("Small"));
-        sizeMenu.add(new JMenuItem("Medium"));
-        sizeMenu.add(new JMenuItem("Large"));
-        sizeMenu.add(new JMenuItem("Extra Large"));
-
-        colorMenu = new JMenu("Color");
-        colorMenu.add(new JMenuItem("Black"));
-        colorMenu.add(new JMenuItem("Red"));
-        colorMenu.add(new JMenuItem("Green"));
-        colorMenu.add(new JMenuItem("Blue"));
-
-        fontMenu = new JMenu("Font");
-        fontMenu.add(new JMenuItem("Font 1"));
-        fontMenu.add(new JMenuItem("Font 2"));
-        fontMenu.add(new JMenuItem("Font 3"));
-
-        // Add submenus to the Config menu
-        configMenu.add(sizeMenu);
-        configMenu.add(colorMenu);
-        configMenu.add(fontMenu);
-
-        // Add File and Config menus to the menu bar
-        menuBar.add(fileMenu);
-        menuBar.add(configMenu);
-    }
-}
+ import javax.swing.*;
+ 
+ public class MobileDeviceV3 extends MobileDeviceV2 {
+     // Declare the JMenuBar and JMenu objects for menu and submenus
+     protected JMenuBar menuBar;
+     protected JMenu fileMenu, configMenu, sizeMenu, colorMenu, fontMenu;
+ 
+     // Declare JMenuItem objects
+     protected JMenuItem newItem, openItem, saveItem, exitItem,smallSizeItem, mediumSizeItem, 
+     largeSizeItem, extraLargeSizeItem,blackColorItem, redColorItem, greenColorItem, blueColorItem,
+     font1Item, font2Item, font3Item;
+ 
+     // Constructor that passes title to the superclass
+     public MobileDeviceV3(String title) {
+         super(title);
+     }
+ 
+     // Method to create and show the GUI
+     public static void createAndShowGUI() {
+         MobileDeviceV3 mdv3 = new MobileDeviceV3("Mobile Device V3");
+ 
+         mdv3.addComponents(); // Add components like buttons and menus
+         mdv3.setFrameFeature(); // Set frame properties like size, close operation, etc.
+     }
+ 
+     // Main method to invoke the createAndShowGUI method in the Event Dispatch Thread
+     public static void main(String[] args) {
+         SwingUtilities.invokeLater(new Runnable() {
+             public void run() {
+                 createAndShowGUI();
+             }
+         });
+     }
+ 
+     // Override addComponents to add the menu bar to the existing layout
+     @Override
+     protected void addComponents() {
+         super.addComponents(); // Call the superclass method to retain existing components
+         addMenus(); // Add the menu bar and menus
+         this.setJMenuBar(menuBar); // Set the created menu bar to the frame
+     }
+ 
+     // Method to create the menu bar and add menus/submenus
+     protected void addMenus() {
+         menuBar = new JMenuBar(); // Create a new menu bar
+ 
+         // Create and add File menu with items
+         fileMenu = new JMenu("File");
+         newItem = new JMenuItem("New");
+         openItem = new JMenuItem("Open");
+         saveItem = new JMenuItem("Save");
+         exitItem = new JMenuItem("Exit");
+ 
+         fileMenu.add(newItem);
+         fileMenu.add(openItem);
+         fileMenu.add(saveItem);
+         fileMenu.add(exitItem);
+ 
+         // Create and add Config menu with submenus (Size, Color, Font)
+         configMenu = new JMenu("Config");
+ 
+         sizeMenu = new JMenu("Size");
+         smallSizeItem = new JMenuItem("Small");
+         mediumSizeItem = new JMenuItem("Medium");
+         largeSizeItem = new JMenuItem("Large");
+         extraLargeSizeItem = new JMenuItem("Extra Large");
+         sizeMenu.add(smallSizeItem);
+         sizeMenu.add(mediumSizeItem);
+         sizeMenu.add(largeSizeItem);
+         sizeMenu.add(extraLargeSizeItem);
+ 
+         colorMenu = new JMenu("Color");
+         blackColorItem = new JMenuItem("Black");
+         redColorItem = new JMenuItem("Red");
+         greenColorItem = new JMenuItem("Green");
+         blueColorItem = new JMenuItem("Blue");
+         colorMenu.add(blackColorItem);
+         colorMenu.add(redColorItem);
+         colorMenu.add(greenColorItem);
+         colorMenu.add(blueColorItem);
+ 
+         fontMenu = new JMenu("Font");
+         font1Item = new JMenuItem("Font 1");
+         font2Item = new JMenuItem("Font 2");
+         font3Item = new JMenuItem("Font 3");
+         fontMenu.add(font1Item);
+         fontMenu.add(font2Item);
+         fontMenu.add(font3Item);
+ 
+         // Add submenus to the Config menu
+         configMenu.add(sizeMenu);
+         configMenu.add(colorMenu);
+         configMenu.add(fontMenu);
+ 
+         // Add File and Config menus to the menu bar
+         menuBar.add(fileMenu);
+         menuBar.add(configMenu);
+     }
+ }
+ 
--- potjanapimol/pattaraporn/lab9/MobileDeviceV4.java
+++ potjanapimol/pattaraporn/lab9/MobileDeviceV4.java
@@ -1,7 +1,77 @@
+/*
+ * Author: Pattaraporn Potjanapimol
+ * ID : 673040627-5
+ * Sec : 2 
+ * 
+ * This class extends MobileDeviceV3 by adding icons to menu items 
+ * and customizing fonts and colorsอ
+ * Inherits all features from MobileDeviceV3.
+ * Adds icons to New, Open, Save, and Exit menu items.
+ * Changes text size for different size options
+ * Changes text color for different color options
+ * Uses different fonts for the font selection menu
+ * 
+ * Last-Update : 07/02/2568
+ */
+
 package potjanapimol.pattaraporn.lab9;
+
+import javax.swing.*;
+import java.awt.*;
 
 import potjanapimol.pattaraporn.lab8.MobileDeviceV3;
 
-public class MobileDeviceV4 extends MobileDeviceV3{
-    
+public class MobileDeviceV4 extends MobileDeviceV3 {
+    public MobileDeviceV4(String title) {
+        super(title);
+    }
+
+    // Method to create and show the GUI
+    public static void createAndShowGUI() {
+        MobileDeviceV4 mdv4 = new MobileDeviceV4("Mobile Device V4");
+
+        mdv4.addComponents(); // Add components like buttons and menus
+        mdv4.setFrameFeature(); // Set frame properties like size, close operation, etc.
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    @Override
+    protected void addComponents() {
+        super.addComponents();
+    }
+
+    // Add menu items with icons and font settings
+    @Override
+    protected void addMenus() {
+        super.addMenus();
+        // Set icons
+        newItem.setIcon(new ImageIcon(getClass().getClassLoader().getResource("images\\new_icon.png")));
+        openItem.setIcon(new ImageIcon(getClass().getClassLoader().getResource("images\\open_icon.png")));
+        saveItem.setIcon(new ImageIcon(getClass().getClassLoader().getResource("images\\save_icon.png")));
+        exitItem.setIcon(new ImageIcon(getClass().getClassLoader().getResource("images\\exit_icon.png")));
+
+        // Set sizes
+        smallSizeItem.setFont(new Font("Arial", Font.PLAIN, 10));
+        mediumSizeItem.setFont(new Font("Arial", Font.PLAIN, 14));
+        largeSizeItem.setFont(new Font("Arial", Font.PLAIN, 18));
+        extraLargeSizeItem.setFont(new Font("Arial", Font.PLAIN, 22));
+
+        // Set colors
+        blackColorItem.setForeground(Color.BLACK);
+        redColorItem.setForeground(Color.RED);
+        greenColorItem.setForeground(Color.GREEN);
+        blueColorItem.setForeground(Color.BLUE);
+
+        // Set fonts
+        font1Item.setFont(new Font("Serif", Font.PLAIN, 14));
+        font2Item.setFont(new Font("SansSerif", Font.PLAIN, 14));
+        font3Item.setFont(new Font("Monospaced", Font.PLAIN, 14));
+    }
 }
